f875a4297eb322ba60320bbdf600e67cad6c8afe
Kamil <kamil.woloszyn1@gmail.com>
2020-09-02 21:45:46 +0200

    Uncomment printf method

--- slice-pointers/danger.go
+++ slice-pointers/danger.go
@@ -7,14 +7,14 @@ func main() {
 	for i := range baseSlice {
 		baseSlice[i] = i
 	}
-	//fmt.Printf("baseSlice: %v with len: %d and cap: %d\n", baseSlice, len(baseSlice), cap(baseSlice))
+	fmt.Printf("baseSlice: %v with len: %d and cap: %d\n", baseSlice, len(baseSlice), cap(baseSlice))
 	one := &baseSlice[1]
-	//	fmt.Printf("Borrowed value with addr: %p and value : %d\n", &one, *one)
+	fmt.Printf("Borrowed value with addr: %p and value : %d\n", &one, *one)
 
 	for i := 0; i < 20; i++ {
 		baseSlice = append(baseSlice, i)
 	}
-	//fmt.Printf("baseSlice expanded. Now len: %d and cap: %d\n", len(baseSlice), cap(baseSlice))
+	fmt.Printf("baseSlice expanded. Now len: %d and cap: %d\n", len(baseSlice), cap(baseSlice))
 	*one = 100
 	fmt.Printf("Changed value of borrowed slice: Now value is %d and value of baseSlice is : %d \naddr: %p != %p", *one, baseSlice[1], &one, &baseSlice[1])
 }
