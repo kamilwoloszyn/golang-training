477171242b4fcbc2c6769761df49d3d0f424fcb8
Kamil <kamil.woloszyn1@gmail.com>
2020-10-19 19:10:50 +0200

    Added println func

--- semantics/main.go
+++ semantics/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type User struct {
 	name  string
 	email string
@@ -24,10 +26,31 @@ func (u *User) CreateUserByPointerSemantics(name string, email string) *User {
 	return &user //allocated on the heap
 }
 
+func (u User) changeUserNameByValueSemantics(name string) {
+	u.name = name
+
+}
+func (u *User) changeUserNameByPointerSemantics(name string) {
+	u.name = name
+}
+
+func (u User) displayName() {
+	fmt.Println(u.name)
+}
+
 //go:noinline
 func main() {
 	var kamil User
 	var kamila *User
 	kamil.CreateUserByValueSemantics("Kamil", "kamil@example.com")
 	kamila.CreateUserByPointerSemantics("Kamila", "kamila@example.com")
+
+	f1 := kamil.changeUserNameByValueSemantics
+	fn2 := kamil.changeUserNameByPointerSemantics
+
+	fn2("Marysia")
+	kamil.displayName()
+
+	f1("Zosia")
+	kamil.displayName()
 }
