86ea456525041189844365be2cd85c2261508e54
Kamil <kamil.woloszyn1@gmail.com>
2020-12-14 09:49:42 +0100

    Fixed null pointer error

--- testing/example1/download_test.go
+++ testing/example1/download_test.go
@@ -11,32 +11,53 @@ import (
 const succeed = "\u2713"
 const failed = "\u2717"
 
+type TestParams struct {
+	name   string
+	urlStr string
+}
+
 func TestDownload(t *testing.T) {
-	var urlStr string = "http://ipv4.download.thinkbroadband.com/5MB.zip"
-	urlParsed, _ := url.Parse(urlStr)
-	path := urlParsed.Path
-	segments := strings.Split(path, "/")
-	fileName := segments[len(segments)-1]
 
-	file, _ := os.Create(fileName)
-	client := http.Client{
-		CheckRedirect: func(r *http.Request, via []*http.Request) error {
-			r.URL.Opaque = r.URL.Path
-			return nil
-		},
-	}
-	t.Log("Should download file with correct paramter given")
-	if _, err := Download(urlStr, file, &client); err == nil {
-		t.Logf("\t %s Download file success ", succeed)
-	} else {
-		t.Fatalf("\t %s Download file FAILED", failed)
+	testParams := []TestParams{
+		{"Good Params", "http://ipv4.download.thinkbroadband.com/5MB.zip"},
+		{"Good Params", "http://ipv4.download.thinkbroadband.com/10MB.zip"},
 	}
 
-	// t.Log("Should cause err")
+	for _, e := range testParams {
+		t.Logf("Running test :  %s", e.name)
+		testingFunc := func(t *testing.T) {
 
-	// if _, err := Download("null", file, &client); err == nil {
-	// 	t.Fatalf("\t %s Test failed", failed)
-	// } else {
-	// 	t.Logf("\t %s, Test succeed", succeed)
-	// }
+			var urlStr string = "http://ipv4.download.thinkbroadband.com/5MB.zip"
+			urlParsed, _ := url.Parse(urlStr)
+			path := urlParsed.Path
+			segments := strings.Split(path, "/")
+			fileName := segments[len(segments)-1]
+
+			file, _ := os.Create(fileName)
+			client := http.Client{
+				CheckRedirect: func(r *http.Request, via []*http.Request) error {
+					r.URL.Opaque = r.URL.Path
+					return nil
+				},
+			}
+			_, err := Download(e.urlStr, file, &client)
+			switch e.name {
+			case "Good Params":
+				if err == nil {
+					t.Logf("Test %s succeed", e.name)
+				} else {
+					t.Fatalf("Test %s failed", e.name)
+				}
+			case "Bad Params":
+				if err == nil {
+					t.Fatalf("Test %s failed", e.name)
+				} else {
+					t.Logf("Test %s succeed", e.name)
+				}
+
+			}
+
+		}
+		t.Run(e.name, testingFunc)
+	}
 }
